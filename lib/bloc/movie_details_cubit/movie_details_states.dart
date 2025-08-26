ccfa21fb8230389abe62cdde89fc55d855a82d19
mury66 <amer66ammar@gmail.com>
2025-08-26 09:15:28 +0300

    finishing movie details

---
+++ lib/bloc/movie_details_cubit/movie_details_states.dart
@@ -0,0 +1,8 @@
+abstract class ExploreStates{}
+class ExploreInitialState extends ExploreStates{}
+
+class ExploreGetCategoryMoviesLoadingState extends ExploreStates{}
+class ExploreGetCategoryMoviesSuccessState extends ExploreStates{}
+class ExploreGetCategoryMoviesErrorState extends ExploreStates{}
+
+class ExploreChangeCategoryIndexState extends ExploreStates{}
