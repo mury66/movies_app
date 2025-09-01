4595da5d3d902c52209984ab7cb429a107c168e1
mury66 <amer66ammar@gmail.com>
2025-09-01 21:12:32 +0300

    no message

--- lib/widgets/movie_list_item.dart
+++ lib/widgets/movie_list_item.dart
@@ -17,11 +17,7 @@ class MovieListItem extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-<<<<<<< HEAD
             builder: (context) => MovieDetailsScreen(movieId:movie.id??550),
-=======
-            builder: (context) => MovieDetailsScreen(movieId: movie?.id ?? 0),
->>>>>>> 9aa478207a924c93a0ab081b4bfd251a53d5befd
           ),
         );
       },
@@ -33,19 +29,14 @@ class MovieListItem extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12.r),
               image: DecorationImage(
-<<<<<<< HEAD
-                image: NetworkImage(movie.mediumCoverImage ??"https://via.placeholder.com/150"),
-=======
                 image: NetworkImage(
-                  movie?.mediumCoverImage ?? "https://via.placeholder.com/150",
+                  movie.mediumCoverImage ?? "https://via.placeholder.com/150",
                 ),
->>>>>>> 9aa478207a924c93a0ab081b4bfd251a53d5befd
                 fit: BoxFit.cover,
               ),
             ),
           ),
           Container(
-<<<<<<< HEAD
               margin: EdgeInsets.only(top : 12.w , left:10.h),
               padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 2.h),
               decoration: BoxDecoration(
@@ -61,32 +52,8 @@ class MovieListItem extends StatelessWidget {
                   Icon(Icons.star, color: Theme.of(context).colorScheme.primary, size: 16.sp,)
                 ],
               )
-          )
-=======
-            margin: EdgeInsets.only(top: 12.w, left: 10.h),
-            padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 2.h),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary.withAlpha(80),
-              borderRadius: BorderRadius.circular(10.r),
+
             ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  movie.rating.toString(),
-                  style: Theme.of(
-                    context,
-                  ).textTheme.headlineSmall!.copyWith(fontSize: 16.sp),
-                ),
-                Icon(
-                  Icons.star,
-                  color: Theme.of(context).colorScheme.primary,
-                  size: 16.sp,
-                ),
-              ],
-            ),
-          ),
->>>>>>> 9aa478207a924c93a0ab081b4bfd251a53d5befd
         ],
       ),
     );
